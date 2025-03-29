46e0f80e8227410e36826dfa5626d1a646aaa122
pyth0g <lenart.kladnik@gmail.com>
2025-03-30 00:21:45 +0100

    VenvSelect

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    branch = "regexp",
+    keys = {
+      { ",v", "<cmd>VenvSelect<cr>" },
+    },
+    ---@type venv-selector.Config
+    opts = {
+      name = ".venv"
+    },
+  },
+}
